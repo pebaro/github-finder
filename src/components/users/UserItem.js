005fe955830620fab7c3819e199e9027036ae425
Robert Masters <rob.masters@pebaro.co.uk>
2022-06-14 16:49:41 +0100

    removing old code

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,16 +1,7 @@
 import React, { Component, Fragment } from 'react'
 
 class UserItem extends Component {
-
-	// state = {
-	// 	id: 'id',
-	// 	login: 'mojombo',
-	// 	avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-	// 	html_url: 'https://github.com/mojombo'
-	// }
-
 	render() {
-		// const { login, avatar_url, html_url } = this.state
 		const { login, avatar_url, html_url } = this.props.singleUser
 
 		return (
